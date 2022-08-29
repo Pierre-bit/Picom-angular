26d2acf07cb5561513ab298a1797197a60fca613
vfrancoil <vfrancoil@hotmail.fr>
2022-08-29 15:50:51 +0200

    finish design diffusions

--- src/app/component/annonce/diffusion/diffusion.component.ts
+++ src/app/component/annonce/diffusion/diffusion.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { Diffusion } from 'src/app/model/diffusion';
 import { AnnonceService } from 'src/app/services/annonce.service';
 import { ArretService } from 'src/app/services/arret.service';
@@ -14,24 +15,25 @@ import { ZonesService } from 'src/app/services/zones.service';
 })
 export class DiffusionComponent implements OnInit {
 
-  diffusions! : Diffusion[];
+  diffusions!: Diffusion[];
 
 
   dataSource = new MatTableDataSource<Diffusion>;
   displayedColumns: string[] = ['id', 'zone', 'arret', 'tarif'];
-  
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
 
   constructor(
-    private diffusion:DiffusionService,
-    private annonceService:AnnonceService,
-    private arretService:ArretService,
-    private zoneService:ZonesService
-    ) { }
+    private diffusion: DiffusionService,
+    private annonceService: AnnonceService,
+    private arretService: ArretService,
+    private zoneService: ZonesService,
+    private router: Router
+  ) { }
 
   ngOnInit(
-    
+
   ): void {
     this.getDiffusion();
     this.getAnnonceById();
@@ -40,22 +42,25 @@ export class DiffusionComponent implements OnInit {
 
   }
 
-  getDiffusion()
-  {
-    this.diffusion.getDiffusionList().subscribe(data =>{
-      this.dataSource.data = data;});
+  getDiffusion() {
+    this.diffusion.getDiffusionList().subscribe(data => {
+      this.dataSource.data = data;
+    });
   }
 
-  getAnnonceById()
-  {
+  getAnnonceById() {
     this.annonceService.getAnnonceListClient(1).subscribe(data => {
       console.log(data);
-      
+
     });
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
+  }
+
+  return() {
+    this.router.navigate(['/annonce'])
   }
 
 }
