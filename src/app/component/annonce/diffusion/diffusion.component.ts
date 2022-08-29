120e61664b0dd3abb4303d684be3e56a4a1b1bac
Pierre-bit <jeanneaupierre@gmail.com>
2022-08-29 12:49:03 +0200

    add diffusion

--- src/app/component/annonce/diffusion/diffusion.component.ts
+++ src/app/component/annonce/diffusion/diffusion.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Diffusion } from 'src/app/model/diffusion';
+import { AnnonceService } from 'src/app/services/annonce.service';
+import { ArretService } from 'src/app/services/arret.service';
 import { DiffusionService } from 'src/app/services/diffusion.service';
+import { ZonesService } from 'src/app/services/zones.service';
 
 @Component({
   selector: 'app-diffusion',
@@ -10,15 +15,47 @@ import { DiffusionService } from 'src/app/services/diffusion.service';
 export class DiffusionComponent implements OnInit {
 
   diffusions! : Diffusion[];
-  constructor(private diffusion:DiffusionService) { }
 
-  ngOnInit(): void {
+
+  dataSource = new MatTableDataSource<Diffusion>;
+  displayedColumns: string[] = ['id', 'zone', 'arret', 'tarif'];
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+
+  constructor(
+    private diffusion:DiffusionService,
+    private annonceService:AnnonceService,
+    private arretService:ArretService,
+    private zoneService:ZonesService
+    ) { }
+
+  ngOnInit(
+    
+  ): void {
     this.getDiffusion();
+    this.getAnnonceById();
+    this.arretService.getArretList();
+    this.zoneService.getZoneList();
+
   }
 
   getDiffusion()
   {
-    this.diffusion.getDiffusionList().subscribe(data =>{ this.diffusions = data});
+    this.diffusion.getDiffusionList().subscribe(data =>{
+      this.dataSource.data = data;});
+  }
+
+  getAnnonceById()
+  {
+    this.annonceService.getAnnonceListClient(1).subscribe(data => {
+      console.log(data);
+      
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
